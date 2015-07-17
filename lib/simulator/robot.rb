ba1fa5622e9735bcfa17e38f926e84f09023886a
Adel Smee <adel.smee@lonelyplanet.com.au>
2015-07-17 16:49:33 +1000

    Add move & report functions, including square exists validation.

--- lib/simulator/robot.rb
+++ lib/simulator/robot.rb
@@ -1,15 +1,21 @@
 module Simulator
   class Robot
     def place new_position
-      change_position new_position
+      new_square = select_xy new_position
+      change_position new_position if Table.has_square? new_square
     end
 
     def move
+      return unless on_table?
+
+      new_square = calculate_coordinates
+      change_position new_square.merge!(direction: current_compass_direction) if Table.has_square? new_square
     end
 
     def turn direction
       return unless on_table?
-      change_position select_coordinates.merge!(direction: calculate_direction(direction))
+
+      change_position current_coordinates.merge!(direction: calculate_direction(direction))
     end
 
     def position
@@ -29,8 +35,19 @@ module Simulator
       @position_log ||= []
     end
 
-    def select_coordinates
-      position.select { |key, value| key.to_s.match /x|y/ }
+    def current_coordinates
+      select_xy position
+    end
+
+    def calculate_coordinates
+      current_position = current_coordinates
+      new_position_key = current_compass_direction.match(/north|south/) ? 'y' : 'x'
+      new_position_value = current_position[new_position_key.to_sym] + 1
+      current_position.merge! new_position_key.to_sym => new_position_value
+    end
+
+    def select_xy position_hash
+      position_hash.select { |key, value| key.to_s.match /x|y/ }
     end
 
     def current_compass_direction
--- spec/simulator/robot_spec.rb
+++ spec/simulator/robot_spec.rb
@@ -2,38 +2,85 @@ require 'spec_helper'
 
 module Simulator
   describe Robot do
+    before(:each) do
+      allow(Simulator::Table).to receive(:has_square?).and_return(true)
+    end
+
     context 'when the robot is not on the table' do
       subject { Robot.new }
+      let(:position) { { x: 2, y: 2, direction: 'south' } }
 
-      it 'executes a valid PLACE command' do
-        subject.place({ x: 2, y: 2, direction: 'south' })
-        expect(subject.position).to eq(x: 2, y: 2, direction: 'south')
+      it 'executes a place command that sits it on the table' do
+        subject.place position
+        expect(subject.position).to eq(position)
       end
 
-      it 'ignores a MOVE command' do
+      it 'ignores a place command does not sit it on the table' do
+        allow(Simulator::Table).to receive(:has_square?).and_return(false)
+        expect(subject.position).to be_nil
+      end
+
+      it 'ignores a move command' do
         subject.move
         expect(subject.position).to be_nil
       end
 
-      it 'ignores a TURN command' do
+      it 'ignores a turn command' do
         subject.turn 'left'
         expect(subject.position).to be_nil
       end
     end
 
     context 'when robot is on the table' do
-      let(:subject) { Robot.new.tap { |robot| robot.place(x: 2, y: 2, direction: 'south') } }
+      let(:subject)            { Robot.new.tap { |robot| robot.place(x: position_x, y: position_y, direction: position_direction) } }
+      let(:position_x)         { 2 }
+      let(:position_y)         { 2 }
+      let(:position_direction) { 'north' }
 
       it 'can turn left' do
         subject.turn 'left'
-        expect(subject.position).to eq(x: 2, y: 2, direction: 'east')
+        expect(subject.position).to eq(x: position_x, y: position_y, direction: 'west')
       end
 
       it 'can turn right' do
         subject.turn 'right'
-        expect(subject.position).to eq(x: 2, y: 2, direction: 'west')
+        expect(subject.position).to eq(x: position_x, y: position_x, direction: 'east')
+      end
+
+      it 'can report current position' do
+        expect(subject.position).to eq(x: position_x, y: position_y, direction: position_direction)
+      end
+
+      describe 'moving' do
+        describe 'to another square on the table' do
+          describe 'on the y axis' do
+            it 'changes position' do
+              subject.move
+              expect(subject.position).to eq(x: position_x, y: (position_y + 1), direction: position_direction)
+            end
+          end
+
+          describe 'on the x axis' do
+            let(:position_direction) { 'east' }
+
+            it 'changes position' do
+              subject.move
+              expect(subject.position).to eq(x: (position_x + 1), y: position_y, direction: position_direction)
+            end
+          end
+        end
+
+        describe 'off the table' do
+          before(:each) do
+            allow(Simulator::Table).to receive(:has_square?).and_return(true, false)
+          end
+
+          it 'does not move' do
+            subject.move
+            expect(subject.position).to eq(x: position_x, y: position_y, direction: position_direction)
+          end
+        end
       end
     end
-
   end
 end
