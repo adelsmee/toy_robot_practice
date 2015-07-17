07a344dc3bc11f137edf09e20331225cfb77a448
Adel Smee <adel.smee@lonelyplanet.com.au>
2015-07-17 17:12:05 +1000

    Implement the Table.

--- lib/simulator/table.rb
+++ lib/simulator/table.rb
@@ -1,4 +1,18 @@
 module Simulator
   class Table
+    class << self
+
+      def has_square? coordinates
+        return false if grid[coordinates[:x]].nil? || grid[coordinates[:x]][coordinates[:y]].nil?
+        true
+      end
+
+      private
+
+      def grid
+        @grid ||= [[0] * 5] * 5
+      end
+
+    end
   end
 end
---
+++ spec/simulator/table_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+module Simulator
+  describe Table do
+    context 'has_square?' do
+      it 'returns true for existing square' do
+        expect(Table.has_square?(x: 1, y: 1)).to be true
+      end
+
+      it 'returns false for non-existent square' do
+        expect(Table.has_square?(x: 100, y: 100)).to be false
+      end
+    end
+  end
+end
