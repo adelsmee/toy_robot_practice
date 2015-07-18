156a0735dbb6118d452dccadfc47c1997e0397cd
Adel Smee <adel.smee@lonelyplanet.com.au>
2015-07-18 15:23:06 +1000

    Make compass directions accessible.

--- lib/simulator/compass.rb
+++ lib/simulator/compass.rb
@@ -1,18 +1,20 @@
 module Simulator
   class Compass
+    DIRECTIONS = ['north','east','south','west']
+
     class << self
 
       def rotate(from, direction)
-        current_index = compass.find_index from
+        current_index = DIRECTIONS.find_index from
         direction_index = convert_direction_to_index direction
         new_index = current_index + direction_index
 
-        if new_index >= 0 && compass[new_index]
-          return compass[new_index]
+        if new_index >= 0 && DIRECTIONS[new_index]
+          return DIRECTIONS[new_index]
         elsif new_index < 0
-          return compass.last
+          return DIRECTIONS.last
         else
-          return compass.first
+          return DIRECTIONS.first
         end
       end
 
@@ -20,10 +22,6 @@ module Simulator
 
       def convert_direction_to_index str_direction
         (str_direction == 'left') ? -1 : 1
-      end
-
-      def compass
-        @compass ||= ['north','east','south','west']
       end
     end
   end
