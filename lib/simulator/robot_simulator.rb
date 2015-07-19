cf765dab021914b27140e7d7bab3e27fb03488bc
Adel Smee <adel.smee@lonelyplanet.com.au>
2015-07-19 20:38:17 +1000

    Implement RobotSimulator script control class. (all tests done but one)

--- lib/simulator/robot_simulator.rb
+++ lib/simulator/robot_simulator.rb
@@ -1,21 +1,45 @@
 module Simulator
   class RobotSimulator
-    def start
-      puts "Enter commands to simulate a toy robot moving on a table"
+    def start debug=false
+      @debug = debug
+      print_instructions
+      simulate
+    end
+
+    private
+
+    def simulate
       while true
         begin
           command = $stdin.gets.strip
+          break if command.downcase == 'quit'
           result = commander.execute command
           puts result if result
         rescue => e
           puts e.message
-          puts e.backtrace
+          puts e.backtrace if @debug
         end
       end
     end
 
+    def print_instructions
+     puts <<-INSTRUCTIONS
+       Enter commands to simulate a toy robot moving on a table.
+       ******************************************************************************
+       Valid commands:
+       PLACE 0,0,NORTH - Places the robot on the table at grid coordinates specified.
+       Valid grid coordinates 0 - 4. **Note** This must be the first command.
+       MOVE - Moves the robot forward one square in whichever direction it is facing.
+       LEFT - Turns the robot one compass point to the left.
+       RIGHT - Turns the robot one compass point to the right.
+       REPORT - Prints the current position of the robot in the format
+              X_COORDINATE,Y_COORDINATE, FACING_COMPASS_DIRECTION.
+       QUIT - Exits the simulation.
+     INSTRUCTIONS
+    end
+
     def commander
-      @commander ||= Command.new(Robot.new)
+      @commander ||= Command.new(Robot.new, @debug)
     end
   end
 end
--- spec/simulator/robot_simulator_spec.rb
+++ spec/simulator/robot_simulator_spec.rb
@@ -2,6 +2,82 @@ require 'spec_helper'
 
 module Simulator
   describe RobotSimulator do
-  end
+    before(:each) do
+      STDOUT.stub(:puts)
+      allow(robot_commander).to receive(:execute)
+      allow(subject).to receive(:print_instructions)
+    end
 
+    subject { RobotSimulator.new }
+    let(:robot_commander) { double }
+
+    describe '#start' do
+      context 'when debug flag is not set' do
+        before(:each) do
+          allow(subject).to receive(:commander).and_return robot_commander
+        end
+
+        context 'with immediate quit' do
+          before(:each) do
+            STDIN.stub(:gets).and_return('QUIT')
+            subject.start
+          end
+
+          it 'prints instructions' do
+            expect(subject).to have_received(:print_instructions)
+          end
+
+          it 'does not pass command to robot' do
+            expect(robot_commander).to_not have_received(:execute)
+          end
+        end
+
+        context 'when command errors' do
+          before(:each) do
+            STDIN.stub(:gets).and_return('MOVE','QUIT')
+            allow(robot_commander).to receive(:execute).and_raise("Bad Things Happened")
+            subject.start
+          end
+
+          it 'passes command to robot' do
+            expect(robot_commander).to have_received(:execute)
+          end
+
+          it 'prints error message with no backtrace' do
+            expect(STDOUT).to have_received(:puts).with("Bad Things Happened")
+          end
+        end
+      end
+
+      context 'when debug flag is set' do
+        describe 'creating robot commander' do
+          before(:each) do
+            STDIN.stub(:gets).and_return('MOVE','QUIT')
+            allow(Command).to receive(:new)
+            subject.start true
+          end
+
+          it 'sets debug flag on Command' do
+            expect(Command).to have_received(:new).with(anything, true)
+          end
+        end
+
+        xcontext 'when command errors' do
+          before(:each) do
+            STDIN.stub(:gets).and_return('MOVE','QUIT')
+            allow(subject).to receive(:commander).and_return robot_commander
+            allow(fake_error).to receive(:backtrace)
+            allow(robot_commander).to receive(:execute).and_raise(fake_error)
+            subject.start true
+          end
+
+          let(:fake_error) { class FakeError < StandardError; end }
+
+          it 'prints backtrace' do
+            expect(fake_error).to have_received(:backtrace)
+          end
+        end
+      end
+    end
+  end
 end
